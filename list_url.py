58d715c383a8bb51fb64249a860d2669544d4ce4
Koki Imai <gonchi282@gmail.com>
2018-10-23 02:07:26 +0900

    hello

--- list_url.py
+++ list_url.py
@@ -4,39 +4,56 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 
-args = sys.argv
+def select_moji(url):
+    list = ["aa", "ba"]
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
+         
+    for i in list:  
+        sample_url = "https://" + i + soup.find("div", class_="img-url").string[3:]
+        req2 = requests.get(sample_url)
+        if req2.status_code == 200:
+            return i
 
-if len(args) < 3:
-    print("Usage %s GalleryNumber initial\n" % args[0])
-    exit(-1)
+    return None
 
-gallery_url = args[1]
-gallery_url = "https://hitomi.la/reader/" + args[1] + ".html"
-gallery_moji  = args[2]
 
-gallery_page = requests.get(gallery_url)
+if __name__ == "__main__":
+    
+    args = sys.argv
 
-if gallery_page.status_code != 200:
-    print("Requests Failed\n")
-    exit(-1)
+    if len(args) < 2:
+        print("Usage %s GalleryNumber initial\n" % args[0])
+        exit(-1)
 
-gallery_soup = BeautifulSoup(gallery_page.text, "html.parser")
+    gallery_url = args[1]
+    gallery_url = "https://hitomi.la/reader/" + args[1] + ".html"
 
-list_imgurl = gallery_soup.findAll("div", class_="img-url")
+    gallery_page = requests.get(gallery_url)
 
-str_imgurl = list()
-changed_imgurl = list()
+    if gallery_page.status_code != 200:
+        print("Requests Failed\n")
+        exit(-1)
 
-#for imgurl in list_imgurl:
-#  str_imgurl.append(imgurl.string)
-str_imgurl = [imgurl.string for imgurl in list_imgurl]
+    gallery_moji = select_moji(gallery_url)
 
-#for imgurl in str_imgurl:
-#  changed_imgurl.append("https://" + gallery_moji + imgurl[3:])
-changed_imgurl = ["https://" + gallery_moji + imgurl[3:] for imgurl in str_imgurl]
+    gallery_soup = BeautifulSoup(gallery_page.text, "html.parser")
 
-# ファイルに書き込み
-with open("img_url.txt", "w") as img_file:
-  for imgurl in changed_imgurl:
-    img_file.write(imgurl + "\n")
+    list_imgurl = gallery_soup.findAll("div", class_="img-url")
+
+    str_imgurl = list()
+    changed_imgurl = list()
+
+    #for imgurl in list_imgurl:
+    #  str_imgurl.append(imgurl.string)
+    str_imgurl = [imgurl.string for imgurl in list_imgurl]
+
+    #for imgurl in str_imgurl:
+    #  changed_imgurl.append("https://" + gallery_moji + imgurl[3:])
+    changed_imgurl = ["https://" + gallery_moji + imgurl[3:] for imgurl in str_imgurl]
+
+    # ファイルに書き込み
+    with open("img_url.txt", "w") as img_file:
+        for imgurl in changed_imgurl:
+            img_file.write(imgurl + "\n")
 
