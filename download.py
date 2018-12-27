40ed8a47546f64c94b9c0929a8545c9b97965217
Koki <gonchi282@gmail.com>
2018-12-27 09:25:27 +0900

    20181227

--- download.py
+++ download.py
@@ -4,21 +4,24 @@ import requests, asyncio, os, sys
 
 def remove_list(list):
     
+    # confirm "[gallerynum]/clearlist.txt" exists
     if os.path.exists(list[0].split("/")[-2] + "/" + "clearlist.txt") == False:
         return list
 
+    # read [gallerynumber]/clearlist.txt
     with open(list[0].split("/")[-2] + "/" + "clearlist.txt", "r") as f:
         urls = f.read()
-    clear_list = urls.split("\n")
-    clear_list.remove("")
+    clear_list = urls.split("\n")   # split by newilne character
+    clear_list.remove("")           # remove blank line
 
-    
+    # remove url that matches urls in clearlist
     for i in clear_list:
         if i in list:
             list.remove(i)
 
     return list
 
+# download a image
 async def download(url):
     print("downloading..." + url)
     req = requests.get(url)
@@ -46,19 +49,21 @@ async def download(url):
     await asyncio.sleep(0.3)
     return True
 
+# gallery directory path  "galleries/[gallerynumber]/"
 gallery_dir = "galleries/" + sys.argv[1] + "/img_url.txt"
     
+# read urls list
 with open(gallery_dir, "r") as f:
     urls = f.read()
 
-url_list = urls.split("\n")
-url_list.remove("")
+url_list = urls.split("\n") # split newline character
+url_list.remove("")         # remove blank line from list
 
 url_list = remove_list(url_list)
 
-loop = asyncio.get_event_loop()
+loop = asyncio.get_event_loop() # async
 
-coros = [download(url) for url in url_list]
+coros = [download(url) for url in url_list] # coroutines
 futures = asyncio.gather(*coros)
 loop.run_until_complete(futures)
 print(futures.result())
