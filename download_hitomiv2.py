e2ace1b9755c06d19b29c63a3648eb04467c9281
Koki Imai <gonchi282@gmail.com>
2019-10-27 21:55:33 +0900

    javascriptでURLを生成するページにも対応
    js_usedに表記を変更

--- download_hitomiv2.py
+++ download_hitomiv2.py
@@ -5,20 +5,66 @@ import urllib.error
 import argparse
 import os
 import sys
+import selenium
 from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 # url_type
 FAILED = 0
-AA_WEBP = 1
-AA = 2
-BA_WEBP = 3
-BA = 4
-CA = 5
+AA = 1
+BA = 2
+CA = 3
+READER = 0
+WEBP = 4
+EXT_WEBP = 8
 
-url = "https://aa.hitomi.la/webp/1449630/025.jpg.webp"
-ref_url = "https://hitomi.la/reader/1450055.html"
+# javascript使用
+JS_USED = 1         # ページにjavascript使用
+JS_NOT_USED = 0     # ページにjavascriptを使用しない
 
+# Referer用URLを取得
+def get_refurl(url):
+    ref_url = url.split("/")
+    ref_url[3] = "reader"
+    ref_url = "/".join(ref_url)
+
+    return ref_url
+
+def chk_disp_page(gallery_url):
+    # ギャラリーページのurlから画像表示ページのurlを推測する
+    display_url = gallery_url.split("/")
+    display_url[3] = "reader"
+    display_url = "/".join(display_url)
+
+    user_agent = "Mozilla/5.0"
+
+    # HTTPリクエストを設定する
+    req = urllib.request.Request(display_url)
+    req.add_header("User-Agent", user_agent)
+    req.add_header("Referer", gallery_url)
+
+    # ページを読み込む
+    try:
+        with urllib.request.urlopen(req) as res:
+            data = res.read()
+    except urllib.error.URLError as e:
+        print(e)
+
+    # スクレイピングする
+    soup = BeautifulSoup(data, "html.parser")
+    div = soup.find("div", class_="img-url")
+
+    if div:
+        ret = JS_NOT_USED
+    else:
+        ret = JS_USED
+
+    return ret
+
+# 画像表示ページから画像URLを取得
 def list_url(gallery_url):
     # ギャラリーページのurlから画像表示ページのurlを推測する
     display_url = gallery_url.split("/")
@@ -45,92 +91,152 @@ def list_url(gallery_url):
 
     # url_typeを決定する line:35～77 ※改善できそう
     url_type = FAILED
-    end_flag = None
+    end_flag = False
+    # aa.hitomi.la か ba.hitomi.laか
     for head in ["aa", "ba", "ca"]:
-        # aa.hitomi.la か ba.hitomi.laか
-        for ext in ["webp", ""]:
-            # webpがつくかつかないか
-            sample = urllist[0].split(".")
-            sample[0] = "https://" + head
-            if not ext == "":
-                sample.append(ext)
-            sample = ".".join(sample)
+        for reader in ["galleries", "webp"]:
+            # 画像が保存されているディレクトリ
+            for ext in ["webp", ""]:
+                # webpがつくかつかないか
+                sample = urllist[0].split(".")
+                sample[0] = "https://" + head
+                sample = ".".join(sample)
+                sample = sample.split("/")
+                sample[3] = reader
+                sample = "/".join(sample)
 
-            req = urllib.request.Request(sample)
-            req.add_header("User-Agent", user_agent)
-            req.add_header("Referer", display_url)
-
-            try:
-                ref = urllib.request.urlopen(req)
-            except urllib.error.URLError as e:
-                continue
-
-            ref.close()
-
-            # ページ取得成功した場合url_typeを決定する
-            if head == "aa":
                 if ext == "webp":
-                    url_type = AA_WEBP
-                else:
-                    url_type = AA
-            elif head == "ba":
+                    sample += ".webp"
+
+                req = urllib.request.Request(sample)
+                req.add_header("User-Agent", user_agent)
+                req.add_header("Referer", display_url)
+
+                try:
+                    ref = urllib.request.urlopen(req)
+                except urllib.error.URLError as e:
+                    continue
+                
+                end_flag = True
+                ref.close()
+
+                # ページ取得成功した場合url_typeを決定する
                 if ext == "webp":
-                    url_type = BA_WEBP
+                    url_type = EXT_WEBP
                 else:
-                    url_type = BA
-            elif head == "ca":
-            	url_type = CA
-            else:
-                url_type = FAILED
-            # 終了フラグを立てる
-            end_flag = True
-            break;
+                    url_type = FAILED
+                
+                if reader == "reader":
+                    url_type |= READER
+                elif reader == "webp":
+                    url_type |= WEBP
+                else:
+                    url_type = FAILED
+                
+                if head == "aa":
+                    url_type |= AA
+                elif head == "ba":
+                    url_type |= BA
+                elif head == "ca":
+                    url_type |= CA
+                else:
+                    url_type = FAILED
+                
+                # url_typeを決定したのでfor文を抜ける
+                break
 
         # 終了フラグが立っている場合は終了する
         if end_flag == True:
-            break;
+            break
 
     # https://xx.hitomi.la…(.webp)の形にする
     urllist2 = list()
-    if url_type == FAILED:  # 失敗
-        print("FAILED")
+    if url_type == FAILED:  # 失敗の場合
+        print("Get URL list FAILED")
         return None
-    elif url_type == AA_WEBP: # aa.hitomi.la….webp
-        for url in urllist:
-            url2 = url.split(".")
-            url2[0] = "https://aa"
-            url2.append("webp")
-            url2 = ".".join(url2)
-            urllist2.append(url2)
-    elif url_type == AA: # aa.hitomi.la…
-        for url in urllist:
-            url2 = url.split(".")
-            url2[0] = "https://aa"
-            url2 = ".".join(url2)
-            urllist2.append(url2)
-    elif url_type == BA_WEBP: # ba.hitomi.la….webp
-        for url in urllist:
-            url2 = url.split(".")
-            url2[0] = "https://ba"
-            url2.append("webp")
-            url2 = ".".join(url2)
-            urllist2.append(url2)
-    elif url_type == BA: # aa.hitomi.la…
-        for url in urllist:
-            url2 = url.split(".")
-            url2[0] = "https://ba"
-            url2 = ".".join(url2)
-            urllist2.append(url2)
-    elif url_type == CA:
-    	for url in urllist:
-    		url2 = url.split(".")
-    		url2[0] = "https://ca"
-    		url2 = ".".join(url2)
-    		urllist2.append(url2)
+    
+    # URLタイプによってURLを変える
+    if url_type & AA:
+        url_head = "https://aa"
+    elif url_type & BA:
+        url_head = "https://ba"
+    else:
+        url_head = "https://ca"
 
+    if url_type & WEBP:
+        url_reader = "webp"
+    else:
+        url_reader = "galleries"
+
+    # URLをリストに追加する
+    for url in urllist:
+        url2 = url.split(".")
+        url2[0] = url_head
+        url2 = ".".join(url2)
+        url2 = url2.split("/")
+        url2[3] = url_reader
+        url2 = "/".join(url2)
+        if url_type & EXT_WEBP:
+            url2 += ".webp"
+        urllist2.append(url2)
+        
     return urllist2
 
-def download(img_url, ref_url):
+# ページがjavascriptで画像URLを生成している場合に使用
+def list_url2(gallery_url):
+    urllist = list()    # URLリスト
+
+    # ギャラリーページのurlから画像表示ページのurlを推測する
+    display_url = gallery_url.split("/")
+    display_url[3] = "reader"
+    display_url = "/".join(display_url)
+    display_url = display_url + "#1"
+    
+    # Chromeを起動
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(chrome_options=options)
+
+    # 最初の画像ページを取得
+    driver.get(display_url)
+    data = driver.page_source
+
+    # 最初のページの画像URLを取得
+    soup = BeautifulSoup(data, "html.parser")
+    image = soup.find("div", id="comicImages").find("img").get("src")
+    urllist.append("https:" + image)
+
+    # 画像数を取得
+    size = len(soup.find("select", id="single-page-select"))
+
+    for i in range(2, size + 1):
+        # 画像表示ページのURLを生成
+        url = display_url.split("#")
+        url[-1] = str(i)
+        url = "#".join(url)
+
+        # 画像ファイルのURLスクレイピング
+        driver.get(url)
+        data = driver.page_source
+        soup = BeautifulSoup(data, "html.parser")
+        div = soup.find("div", id="comicImages")
+        image = div.find("img")
+        image = soup.find("div", id="comicImages").find("img").get("src")
+        urllist.append("https:" + image)
+
+        if i == size:
+            print("\r{} / {}".format(i, size))          # 最後は改行
+        else:
+            print("\r{} / {}".format(i, size), end="")
+    
+
+    # Chromeを終了
+    driver.close()
+
+    return urllist
+
+# 画像をダウンロード
+def download(img_url, ref_url, num):
     # User Agent
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
 
@@ -149,7 +255,7 @@ def download(img_url, ref_url):
         return False
     else:
         # ギャラリー番号
-        gallery_num = img_url.split("/")[-2]
+        gallery_num = ref_url.split("/")[-1].split(".")[0]
 
         # ギャラリーのディレクトリがない場合作成する
         if not os.path.exists("galleries/" + gallery_num):
@@ -159,19 +265,20 @@ def download(img_url, ref_url):
         fname = img_url.split("/")[-1]
         ext = fname.split(".")[-1]
         if ext == "webp":
-            # 拡張子がwebp
-            remove_webp = fname.split(".")
-            del remove_webp[-1]
-            fname = ".".join(remove_webp)
+            ext = fname.split(".")[-2]
+        
+        fname = str(num) + "." + ext
+
         # ファイルに書き込む
         with open("galleries/" + gallery_num + "/" + fname, "wb") as f:
             f.write(data)
 
         return True
 
-def exec_download(img_url, ref_url):
+# ダウンロード実行用関数
+def exec_download(img_url, ref_url, num):
     print("downloading..." + img_url)
-    ret = download(img_url, ref_url)
+    ret = download(img_url, ref_url, num)
     return ret
 
 def main():
@@ -181,13 +288,26 @@ def main():
     parser.add_argument("-t", default=25, help="thread count")  # 稼働スレッド数
     args = parser.parse_args()
 
+    # HTTP Referer用URL
+    ref_url = get_refurl(args.url)
+
+    print("Check displaypage")
+    js_used = chk_disp_page(args.url)
+
     # 画像のURLをリスト化
-    urllist = list_url(args.url)
-    print(args.url)
+    if js_used:
+        urllist = list_url2(args.url)
+    else:
+        urllist = list_url(args.url)
+
+    # 画像URL取得失敗
+    if urllist is None:
+        print("List Failed")
+        sys.exit()
 
     # マルチスレッドで画像をダウンロード
     executor = ThreadPoolExecutor(max_workers=int(args.t))
-    futures = [executor.submit(exec_download, url, ref_url) for url in urllist]
+    futures = [executor.submit(exec_download, url, ref_url, i) for url, i in zip(urllist, range(len(urllist)))]
 
 
 if __name__ == "__main__":
